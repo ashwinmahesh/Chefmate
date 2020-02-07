0edcea2a398e34d8b733889e6db18e484a26f90b
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-06 19:23:39 -0500

    Added react app

---
+++ client/frontend/src/template/template.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+function Template() {
+  const styles = useStyles();
+
+  return(
+    <div>
+      <p>I am a template</p>
+    </div>
+  )
+}
+
+export default Template;
