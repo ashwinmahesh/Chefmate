a3dc97edb0894559a77cef408b417bb838049a17
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-11 14:43:06 -0500

    Further setup server.js

--- client/server.js
+++ client/server.js
@@ -3,3 +3,34 @@ if(process.env.NODE_ENV != 'production') {
 }
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const expressSession = require('express-session')
+const path = require('path')
+const pino = require('express-pino-logger')();
+const fs = require('fs')
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+const root = require('path').join(__dirname, 'frontend', 'build');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession({
+  secret: 'DontTellNobody69',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1*24*60*60*1000
+  }
+}));
+app.use(pino)
+app.use(express.static(root))
+
+app.get('*', (request, response) => {
+  return response.sendFile('index.html', { root });
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+})
