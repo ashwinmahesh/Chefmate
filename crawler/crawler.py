7e3480226db3c1e226229da2e04cdb5e2ba0cffd
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-11 15:14:24 -0500

    Moved app code to crawler.py

---
+++ crawler/crawler.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+# client = MongoClient(<<MONGODB URL>>)
+# db=client.admin
+
+# Issue the serverStatus command and print the results
+# serverStatusResult=db.command("serverStatus")
+# pprint(serverStatusResult)
+
+@app.route('/')
+def index():
+  return 'Hello World!'
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=8001)
