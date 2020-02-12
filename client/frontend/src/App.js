23316d0a3d9faf14b58d13b9e526b78d45ffba33
Richa Tenany <richa.tenany@gmail.com>
2020-02-11 22:45:22 -0500

    split into rows

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,28 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from '@material-ui/core/Box';
 import Template from './template/template'
 
-function App() {
+export default function FlexGrow() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Template />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%' }}>
+      <Box display="flex" p={1} bgcolor="background.paper">
+        <Box p={1} flexGrow={1} bgcolor="grey.300"
+        css={{ maxWidth: 270, height: 200, margin: 2  }}>
+          Item 1
+        </Box>
+        <Box p={1} flexGrow={1} bgcolor="grey.300"
+          css={{ maxWidth: 270, height: 200, margin: 2  }}>
+          Item 2
+        </Box>
+        <Box p={1} flexGrow={1} bgcolor="grey.300"
+        css={{ maxWidth: 270, height: 200, margin: 2  }}>
+          Item 3
+        </Box>
+        <Box p={1} flexGrow={1} bgcolor="grey.300"
+        css={{ maxWidth: 270, height: 200, margin: 2  }}>
+          Item 4
+        </Box>
+        <Box p={1} flexGrow={1} bgcolor="grey.300"
+        css={{ maxWidth: 270, height: 200, margin: 2  }}>
+          Item 5
+        </Box>
+      </Box>
     </div>
   );
 }
 
-export default App;
+// const FlexboxPage = () => (
+//   <>
+  
+//     <div className="d-flex flex-col">
+//       <div className="p-2 col-example text-left">Flex item 1</div>
+//       <div className="p-2 col-example text-left">Flex item 2</div>
+//       <div className="p-2 col-example text-left">Flex item 3</div>
+//     </div>
+   
+//   </>
+// );
+
+
+// export default FlexboxPage;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+       
+//         <Template />
+        
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
+
+
--- client/frontend/src/template/template.js
+++ client/frontend/src/template/template.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
 
-}));
-
-function Template() {
-  const styles = useStyles();
-
-  return(
-    <div>
-      <p>I am a template</p>
-    </div>
-  )
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+export default function Hook() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Hook</Button>;
 }
 
-export default Template;
+// const useStyles = makeStyles(theme => ({
+
+// }));
+
+// function Template() {
+//   const styles = useStyles();
+
+//   return(
+//     <div>
+//       <p>I am a template</p>
+//     </div>
+//   )
+// }
+
+// export default Template;
