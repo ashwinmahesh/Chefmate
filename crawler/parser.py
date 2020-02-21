bfeecc94c2d060334e32b549f99807d3cfd5db7a
vish3418 <pandyav@purdue.edu>
2020-02-21 18:14:49 -0500

    Stopping and stemming works for a single site. Small changes may need to be made depending on how the function is called

--- crawler/parser.py
+++ crawler/parser.py
@@ -2,6 +2,9 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from lxml import html
 import requests
+import nltk
+import string
+import re
 
 class Parser:
 
@@ -9,13 +12,50 @@ class Parser:
         self.baseURL = baseURL
 
     def parse(self, baseURL):
+
         page = urlopen(baseURL).read()
         soup = BeautifulSoup(page, "html.parser")
         parsed = ""
-        for node in soup.findAll('body'):
-            parsed += ('\n'.join(node.findAll(text=True)))
+        parsed2 = ""
+        title = soup.title
+        title = title.string #title contains the title of the page at this point Ex: "Google"
 
-        print(parsed)
+        for node in soup.findAll('body'):
+            parsed = ('\n'.join(node.findAll(text=True)))
+
+        parsed = parsed.splitlines()
+        #print(parsed)
+
+        """For loop parses the text within the document to only have the necessary information. 
+        Parsed2 will contain the shortened version"""
+
+        for s in parsed: 
+            if not('function()' in s or "();" in s): 
+                parsed2 += s
+
+        contents = parsed2.split(' ')
+
+        """stoping and stemming below - contents will contain final list of words after stopping and stemming"""
+
+        nltk.download('stopwords')
+        contents = [content.lower() for content in contents]
+        table = str.maketrans('', '', string.punctuation)
+        contents = [content.translate(table) for content in contents]
+        contents = [re.sub(r'\d+', '', content) for content in contents]
+        stopwords = set(nltk.corpus.stopwords.words('english'))
+        contents = [[word for word in content.split() if word not in stopwords] for content in contents]
+
+        stems = nltk.stem.PorterStemmer()
+        contents = [" ".join([stems.stem(word) for word in content]) for content in contents]
+        contents = list(filter(lambda x : x != '', contents))
+
+        finalContents = []
+        dictionary = dict(link=baseURL, title=title, body=contents)
+        finalContents.append(dictionary)
+       
+        return finalContents
+        
+
         """body = soup.find('body')
         bodyContent = body.findChildren(recursive=False)
         soup2 = BeautifulSoup(bodyContent)
