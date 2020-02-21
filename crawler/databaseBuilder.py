3e432acf6c078bf42a30e0a59a10b8bb47469d50
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-21 16:31:33 -0500

    Changed log function and implemented writing to db using pseudo functions

--- crawler/databaseBuilder.py
+++ crawler/databaseBuilder.py
@@ -1,8 +1,22 @@
+import sys
+sys.path.append('..')
+import helpers
+log = helpers.log
+
+
+#TODO Replace with actual DB write funtions in the future. Using these stubs for now.
+def inDatabase(db, word):
+  return False
+
+def writeToDatabase(dbName, contents, key):
+  _id = key
+  print("Writing to database:", contents)
+
 class DatabaseBuilder:
   def __init__(self, domain):
     self.domain = domain
   
-  def readFile(self):
+  def readFile(self, printStatements=False):
     filePath = 'domains/'+self.domain +'/'+self.domain+"_index.txt" 
     file = open(filePath, 'r')
     for line in file:
@@ -18,9 +32,22 @@ class DatabaseBuilder:
       title = title[7:len(title)-1]
       body = body[6:len(body)-1]
 
-      print(f"{docID}\n{link}\n{title}\n{body}\n")
+      self.addDocumentToCollection(docID, link, title, body)
+      if printStatements:
+        log('entry', docID)
+        log('entry', link)
+        log('entry', title)
+        log('entry', body)
+  
+  def addDocumentToCollection(self, docID, link, title, body):
+    writeToDatabase('Crawler', {'link':link, 'title':title, 'body':body}, docID)
+    
+  def buildInvertedIndex(self, body, docID):
+    for word in body:
+      pass
+
 
 if __name__ == "__main__":
   d = DatabaseBuilder('SimplyRecipes')
-  d.readFile()
+  d.readFile(printStatements=True)
         
