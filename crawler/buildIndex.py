5e1fe32c84cedfe208d22c24e1cbe05fa5cece6b
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-23 22:06:32 -0500

    Updated print statements. Added old domain cleanup

--- crawler/buildIndex.py
+++ crawler/buildIndex.py
@@ -2,6 +2,7 @@ from crawler import Crawler
 from dataParser import DataParser
 from databaseBuilder import DatabaseBuilder
 import time
+import os
 
 domains = [
     {'name': 'Tasty', 'root': 'https://tasty.co/'},
@@ -11,7 +12,11 @@ domains = [
 ]
 
 def buildIndex(iterations):
+  log('build index', 'Running full suite of crawler programs.')
   programStartTime = time.time()
+  if os.path.exists('domains'):
+    log('cleanup', 'Removing old domains folder')
+    os.rmdir('domains')
   for domain in domains:
     domainStartTime = time.time()
     crawler = Crawler(domain['name'], domain['root'])
@@ -26,4 +31,4 @@ def buildIndex(iterations):
   log("time", "Program finished running in "+str(time.time()-programStartTime)+" seconds.")
 
 if __name__ == "__main__":
-  buildIndex(2)
+  buildIndex(1)
