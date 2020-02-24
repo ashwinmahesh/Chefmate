ae79a00854fd9d9344abd5c3ad6c7592ab99264c
Ashwin <mahesh.ashwin1998@gmail.com>
2020-02-23 21:59:08 -0500

    Added timers to buildIndex.py

--- crawler/buildIndex.py
+++ crawler/buildIndex.py
@@ -1,6 +1,7 @@
 from crawler import Crawler
 from dataParser import DataParser
 from databaseBuilder import DatabaseBuilder
+import time
 
 domains = [
     {'name': 'Tasty', 'root': 'https://tasty.co/'},
@@ -10,15 +11,19 @@ domains = [
 ]
 
 def buildIndex(iterations):
+  programStartTime = time.time()
   for domain in domains:
+    domainStartTime = time.time()
     crawler = Crawler(domain['name'], domain['root'])
     crawler.runSpider(iterations)
     dataParser = DataParser(domain['name'])
     dataParser.runParser()
     databaseBuilder = DatabaseBuilder(domain['name'])
     databaseBuilder.build()
+    log("time", domain['name']+" finished running in "+str(time.time()-domainStartTime)+" seconds.")
   
   DatabaseBuilder.calculateIDF()
+  log("time", "Program finished running in "+str(time.time()-programStartTime)+" seconds.")
 
 if __name__ == "__main__":
   buildIndex(2)
