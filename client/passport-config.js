4dc4566ae0c88467f3b90a0777b36d7ebc17f460
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-04 22:42:33 -0500

    Updated print statement, verified that it works to store in db

--- client/passport-config.js
+++ client/passport-config.js
@@ -3,6 +3,7 @@ const { User } = require('./mongoConfig');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const log = require('./logger');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -25,11 +26,12 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
-      User.findOne({ userid: profile.id }).then((existingUser) => {
+      User.findOne({ userid: profile.emails[0].value }).then((existingUser) => {
         if (existingUser) {
+          log("login", "Found existing user")
           done(null, existingUser);
         } else {
-          console.log('No user exists');
+          log("login", 'No user exists')
           new User({
             userid: profile.emails[0].value
           }).save().then((user) => done(null, user));
