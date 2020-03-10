ad1f7232d33bd12592b3cb7cdadc6061adbe824d
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-10 16:53:14 -0400

    Update cosine similarity to use numpy and added print statements

--- ranker/cosineSimilarity.py
+++ ranker/cosineSimilarity.py
@@ -6,25 +6,23 @@ from mongoConfig import *
 from helpers import log
 import math
 import numpy as np
+import time
 
 # cos(D1, Q) = dot_product(TFIDF1, TFIDFq) / sqrt(sum(TFIDF1^2) * sum(TFIDFq^2))
-def cosineSimilarity(termWeights1, termWeights2,):
+def cosineSimilarity(termWeights1, termWeights2):
   if len(termWeights1) != len(termWeights2):
     print("Error: Lengths of weight vectors do not match")
     return -1
 
-  top = 0.0
-  sumSquared1 = 0.0
-  sumSquared2 = 0.0
-  for i in range(0, len(termWeights1)):
-    top+=termWeights1[i]*termWeights2[i]
-    sumSquared1+=pow(termWeights1[i], 2)
-    sumSquared2+=pow(termWeights2[i], 2)
-  
-  bottom = math.sqrt(sumSquared1*sumSquared2)
+  top = np.dot(termWeights1, termWeights2.T)
+  magnitude1 = np.dot(termWeights1, termWeights1.T)
+  magnitude2 = np.dot(termWeights2, termWeights2.T)
+  bottom = math.sqrt(magnitude1*magnitude2)
+
   return top/bottom
 
 def calculateAllCosineSimilarity(terms, inMemoryTFIDF):
+  startTime = time.time()
   connect('chefmateDB', host='18.222.251.5', port=27017)
   docIds = set()
   queryTermWeights = np.zeros(InvertedIndex.objects.count())
@@ -42,8 +40,10 @@ def calculateAllCosineSimilarity(terms, inMemoryTFIDF):
   for docId in docIds:
     docWeight = inMemoryTFIDF[:,docId]
     cosSim = cosineSimilarity(queryTermWeights, docWeight)
+    log('cosine', 'Cosine similarity with doc '+str(docId)+' = '+str(cosSim))
     cosineSimilarities.append(cosSim)
   
+  log('time', 'Execution time for cosine similarities: ' +str(time.time()-startTime)+' seconds')
   return cosineSimilarities
 
 if __name__ =="__main__":
