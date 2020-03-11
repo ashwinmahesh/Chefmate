1d7730b21cec89f4b482fdc54bf39f19810c7f7f
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-10 23:04:14 -0400

    Finished UI, moving Single Result into separate file

--- client/frontend/src/SearchResultPage/Results.js
+++ client/frontend/src/SearchResultPage/Results.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
+
+// import { ThumbUpAltIcon } from '@material-ui/icons/ThumbUpAltIcon';
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
@@ -29,10 +33,32 @@ const useStyles = makeStyles((theme) => ({
   singleSiteContainer: {
     marginBottom: '30px',
   },
+  likeCount: {
+    color: 'green',
+    margin: '0px',
+    display: 'inline-block',
+    marginLeft: '20px',
+  },
+  thumbsUp: {
+    fontSize: '14pt',
+    color: 'green',
+  },
+  thumbsDown: {
+    fontSize: '14pt',
+    color: 'rgb(210, 0, 0)',
+  },
 }));
 
 function Results() {
   const styles = useStyles();
+  function likePressed() {
+    console.log('Like button pressed');
+  }
+
+  function dislikePressed() {
+    console.log('Dislike button pressed');
+  }
+
   function renderSingleSiteInfo() {
     return (
       <div className={styles.singleSiteContainer}>
@@ -44,6 +70,15 @@ function Results() {
           Hello i am random sample text for google. This is text related to what you
           have searched for. Blah blah blah here is some more text.
         </p>
+        <div>
+          <IconButton aria-label="Like" onClick={likePressed}>
+            <FaThumbsUp className={styles.thumbsUp} />
+          </IconButton>
+          <IconButton aria-label="Dislike" onClick={dislikePressed}>
+            <FaThumbsDown className={styles.thumbsDown} />
+          </IconButton>
+          <p className={styles.likeCount}>3300 users liked this page.</p>
+        </div>
       </div>
     );
   }
