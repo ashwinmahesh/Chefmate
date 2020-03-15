d88a1fb0e2873d0158c09d1c181f81fdb37e764e
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-15 10:41:17 -0700

    Cleanup

--- crawler/graph.py
+++ crawler/graph.py
@@ -11,16 +11,3 @@ class Graph:
   def printGraph(self):
     for node in self.nodes:
       print(node, ':', self.nodes[node])
-
-
-class OutlinkNode:
-  def __init__(self, nodeUrl):
-    self.nodeUrl=nodeUrl
-    self.outlinks=set()
-  
-  def addOutlink(self, link):
-    if link != nodeUrl:
-      self.outlinks.add(link)
-  
-  def getOutlinks(self):
-    return self.outlinks
