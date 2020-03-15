af47a2ff0dfa15bf0495dbfe3d2cc452ce6cf371
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-15 10:43:37 -0700

    Added get function to graph

--- crawler/graph.py
+++ crawler/graph.py
@@ -8,6 +8,11 @@ class Graph:
     if nodeUrl != link:
       self.nodes[nodeUrl].add(link)
   
+  def get(self, node):
+    if node in self.nodes:
+      return self.nodes[node]
+    return set()
+  
   def printGraph(self):
     for node in self.nodes:
       print(node, ':', self.nodes[node])
