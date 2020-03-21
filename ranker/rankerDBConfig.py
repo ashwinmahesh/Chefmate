35a676bab2452a85fb6f37bdf6219c1ddff28707
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-21 14:12:29 -0700

    Minor change to which db being used

--- ranker/rankerDBConfig.py
+++ ranker/rankerDBConfig.py
@@ -1,5 +1,5 @@
-databaseName = 'ChefmateDB'
-# databaseName = 'ChefmateDB_Alt'
+# databaseName = 'ChefmateDB'
+databaseName = 'ChefmateDB_Alt'
 
 # databaseAddr = '3.21.167.180'
 databaseAddr = '18.222.251.5'
