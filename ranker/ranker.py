41ec57cedf429729ab527b1c1f07ef0c253a0fbe
Ashwin <mahesh.ashwin1998@gmail.com>
2020-03-23 20:33:09 -0700

    Setup ranker for deployment

--- ranker/ranker.py
+++ ranker/ranker.py
@@ -16,7 +16,7 @@ from loadInvertedIndexToMemory import loadInvertedIndexToMemory
 
 app = Flask(__name__)
 
-port = 8002
+port = 8002 if app.config['ENV']=='development' else 80
 connect(rankerDBConfig.databaseName, host=rankerDBConfig.databaseAddr, port=27017)
 
 inMemoryTFIDF, crawlerReverseMap, termReverseMap = loadInvertedIndexToMemory()
@@ -44,8 +44,6 @@ def fetchDocuments():
   log('ranker', 'Finished fetching documents in '+str(time.time() - startTime) + ' seconds')
   return helpers.sendPacket(1, 'Successfully retrieved documents', {'documents':documents})
 
-#Insert Helper functions below here
-
 if __name__ == "__main__":
-  log('info', f"Ranker is listening on port {port}, {app.config['ENV']} environment.")
+  log('info', "Ranker is listening on port "+str(port) +", " + str(app.config['ENV']) + " environment.")
   app.run(debug=True, host='0.0.0.0', port=port)
