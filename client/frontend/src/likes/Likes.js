4ad4d27fb44b9fdd298e6351f71478b7f5a0bc96
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-01 15:42:08 -0400

    Created base for likes page

--- client/frontend/src/likes/Likes.js
+++ client/frontend/src/likes/Likes.js
@@ -3,8 +3,37 @@ import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import HeaderSearch from '../Headers/HeaderSearch';
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  Typography,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { FaStar } from 'react-icons/fa';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    background: 'rgb(40,40,40)',
+  },
+  panelsWrapper: {
+    paddingLeft: '15px',
+    paddingRight: '15px',
+    paddingTop: '20px',
+    paddingBottom: '20px',
+  },
+  expansionPanel: {
+    background: 'rgb(72,72,72)',
+  },
+  titleText: {
+    color: 'white',
+    fontSize: '13pt',
+    marginLeft: '20px',
+  },
+  expandIcon: {
+    color: 'white',
+    size: 16,
+  },
+}));
 
 export default function Likes() {
   const styles = useStyles();
@@ -30,12 +59,39 @@ export default function Likes() {
     fetchUserInfo();
   }, []);
 
+  function renderTestLikes() {
+    const output = [];
+    for (var i = 0; i < 10; i++) {
+      output.push(
+        <ExpansionPanel className={styles.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon color="white" />}
+            classes={{ expandIcon: styles.expandIcon }}
+          >
+            <FaStar size={24} color="gold" />
+            <Typography className={styles.titleText}>Google Images</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+      );
+    }
+    return output;
+  }
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       {loginRedirect && <Redirect to="/" />}
       <HeaderSearch initialSearch="" />
-
-      <p>I am the likes page</p>
+      <div className={styles.panelsWrapper}>
+        {renderTestLikes()}
+        {/* <ExpansionPanel className={styles.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon color="white" />}
+            classes={{ expandIcon: styles.expandIcon }}
+          >
+            <Typography className={styles.titleText}>Google Images</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel> */}
+      </div>
     </div>
   );
 }
