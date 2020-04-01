b9b16a8d3c7098186fd966d2b3937168c2bf61b8
vish3418 <pandyav@purdue.edu>
2020-04-01 19:16:32 -0400

    Next component for sppech recognition

--- client/frontend/src/voice/Voice.js
+++ client/frontend/src/voice/Voice.js
@@ -10,3 +10,40 @@ const recognition = new SpeechRecognition();
 recognition.continous = true;
 recognition.interimResults = true;
 recognition.lang = 'en-US';
+//------------------------COMPONENT-----------------------------
+
+class Speech extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listening: false,
+    };
+    this.toggleListen = this.toggleListen.bind(this);
+    this.handleListen = this.handleListen.bind(this);
+  }
+
+  toggleListen() {
+    this.setState(
+      {
+        listening: !this.state.listening,
+      },
+      this.handleListen
+    );
+  }
+
+  handleListen() {
+    // handle speech recognition here
+  }
+
+  render() {
+    return (
+      <div style={container}>
+        <button id="microphone-btn" style={button} onClick={this.toggleListen} />
+        <div id="interim" style={interim}></div>
+        <div id="final" style={final}></div>
+      </div>
+    );
+  }
+}
+
+export default Speech;
