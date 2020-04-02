73425c18955661313a1b20be1529d2232a771f67
vish3418 <pandyav@purdue.edu>
2020-04-01 23:41:30 -0400

    Added CSS Component

--- client/frontend/src/voice/Voice.js
+++ client/frontend/src/voice/Voice.js
@@ -47,3 +47,37 @@ class Speech extends Component {
 }
 
 export default Speech;
+
+//-------------------------CSS------------------------------------
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  button: {
+    width: '60px',
+    height: '60px',
+    background: 'lightblue',
+    borderRadius: '50%',
+    margin: '6em 0 2em 0',
+  },
+  interim: {
+    color: 'gray',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px',
+  },
+  final: {
+    color: 'black',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px',
+  },
+};
+
+const { container, button, interim, final } = styles;
