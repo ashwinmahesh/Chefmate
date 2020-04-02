349b86458c5001d111ba1a5dfb312f7548b7adce
vish3418 <pandyav@purdue.edu>
2020-04-01 23:48:05 -0400

    Added commands component

--- client/frontend/src/voice/Voice.js
+++ client/frontend/src/voice/Voice.js
@@ -14,56 +14,91 @@ recognition.lang = 'en-US';
 //------------------------COMPONENT-----------------------------
 
 class Speech extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
-      listening: false
-    }
-    this.toggleListen = this.toggleListen.bind(this)
-    this.handleListen = this.handleListen.bind(this)
+      listening: false,
+    };
+    this.toggleListen = this.toggleListen.bind(this);
+    this.handleListen = this.handleListen.bind(this);
   }
 
   toggleListen() {
-    this.setState({
-      listening: !this.state.listening
-    }, this.handleListen)
+    this.setState(
+      {
+        listening: !this.state.listening,
+      },
+      this.handleListen
+    );
   }
 
   handleListen() {
-
-    console.log('listening?', this.state.listening)
+    console.log('listening?', this.state.listening);
 
     if (this.state.listening) {
-      recognition.start()
+      recognition.start();
       recognition.onend = () => {
-        console.log("...continue listening...")
-        recognition.start()
-      }
-
+        console.log('...continue listening...');
+        recognition.start();
+      };
     } else {
-      recognition.stop()
+      recognition.stop();
       recognition.onend = () => {
-        console.log("Stopped listening per click")
-      }
+        console.log('Stopped listening per click');
+      };
     }
 
     recognition.onstart = () => {
-      console.log("Listening!")
-    }
+      console.log('Listening!');
+    };
 
-    let finalTranscript = ''
-    recognition.onresult = event => {
-      let interimTranscript = ''
+    let finalTranscript = '';
+    recognition.onresult = (event) => {
+      let interimTranscript = '';
 
       for (let i = event.resultIndex; i < event.results.length; i++) {
         const transcript = event.results[i][0].transcript;
         if (event.results[i].isFinal) finalTranscript += transcript + ' ';
         else interimTranscript += transcript;
       }
-      document.getElementById('interim').innerHTML = interimTranscript
-      document.getElementById('final').innerHTML = finalTranscript
+      document.getElementById('interim').innerHTML = interimTranscript;
+      document.getElementById('final').innerHTML = finalTranscript;
 
+      //-------------------------COMMANDS------------------------------------
+
+      const transcriptArr = finalTranscript.split(' ');
+      const stopCmd = transcriptArr.slice(-3, -1);
+      console.log('stopCmd', stopCmd);
+
+      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening') {
+        recognition.stop();
+        recognition.onend = () => {
+          console.log('Stopped listening per command');
+          const finalText = transcriptArr.slice(0, -3).join(' ');
+          document.getElementById('final').innerHTML = finalText;
+        };
+      }
+    };
+
+    //-----------------------------------------------------------------------
+
+    recognition.onerror = (event) => {
+      console.log('Error occurred in recognition: ' + event.error);
+    };
+  }
+
+  render() {
+    return (
+      <div style={container}>
+        <button id="microphone-btn" style={button} onClick={this.toggleListen} />
+        <div id="interim" style={interim}></div>
+        <div id="final" style={final}></div>
+      </div>
+    );
+  }
+}
+
+export default Speech;
 //-------------------------CSS------------------------------------
 
 const styles = {
