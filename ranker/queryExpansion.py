7957cf77ac96bc6d758442ccc07dc507d253fa54
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-23 23:48:41 -0400

    Added todos, done for now

--- ranker/queryExpansion.py
+++ ranker/queryExpansion.py
@@ -12,51 +12,51 @@ from threading import Thread
 
 connect(rankerDBConfig.databaseName, host=rankerDBConfig.databaseAddr, port=27017)
 N = Crawler.objects.count()
-characters = ['(', '.', '$']
-for i in range(0, 9):
-  characters.append(str(i))
-for i in range(ord('a'), ord('z')+1):
-  characters.append(chr(i))
+specialChars = ['\(', '\.', '$']
 
-def queryExpansionWorker(queryDocInfo, terms):
-  # startTime = time.time()
-  # log("QE", 'Performing Query Expansion on ' + query)
+characters = []
+chiSquaredVals = []
+termsList = []
 
-  # queryTerm = json.loads(InvertedIndex.objects.get(term=query).to_json())
-  # queryDocInfo = queryTerm['doc_info']
-
+def queryExpansionWorker(queryDocInfo):
   Na = len(queryDocInfo)
-  Nab, Nb = 0, 0
-  
-  chiSquaredVals = []
-  termsList = []
-  for term in terms:
-    termsList.append(term['_id'])
-    Nab = 0
-    termDocInfo = term['doc_info']
-    Nb = len(termDocInfo)
-    
-    for urlKey in termDocInfo:
-      if urlKey in queryDocInfo:
-        Nab += 1
-    
-    chiSquared = ((Nab - (Na * Nb)/N)**2) / (Na * Nb)
-    chiSquaredVals.append(chiSquared)
+  while(len(characters) > 0):
+    nextChar = characters.pop()
+    regex = re.compile('^'+nextChar, re.IGNORECASE)
+    terms = json.loads(InvertedIndex.objects(term=regex).to_json())
 
-  sortedChiSquaredVals = [(chiSquared, term) for chiSquared, term in sorted(zip(chiSquaredVals, termsList), reverse=True)][0:30]
-  log("time", "Finished performing Query Expansion in " + str(time.time() - startTime) + ' seconds.')
-  return sortedChiSquaredVals
+    Nab, Nb = 0, 0
+    
+    for term in terms:
+      Nab = 0
+      termDocInfo = term['doc_info']
+      Nb = len(termDocInfo)
+      
+      for urlKey in termDocInfo:
+        if urlKey in queryDocInfo:
+          Nab += 1
+      
+      chiSquared = ((Nab - (Na * Nb)/N)**2) / (Na * Nb)
+      termsList.append(term['_id'])
+      chiSquaredVals.append(chiSquared)
 
 def queryExpansion(query, threads=1):
   startTime = time.time()
   log("QE", 'Performing Query Expansion on ' + query)
+
+  for i in range(0, 9):
+    characters.append(str(i))
+  for i in range(ord('a'), ord('z')+1):
+    characters.append(chr(i))
+  for char in specialChars:
+    characters.append(char)
 
   queryTerm = json.loads(InvertedIndex.objects.get(term=query).to_json())
   queryDocInfo = queryTerm['doc_info']
 
   threadPool = []
   for i in range(0, threads):
-    newThread = Thread(name='queryExpansion_'+str(i), target=queryExpansionWorker, args=(queryDocInfo))
+    newThread = Thread(name='queryExpansion_'+str(i), target=queryExpansionWorker, args=(queryDocInfo,))
     threadPool.append(newThread)
   
   for i in range(0, threads):
@@ -64,14 +64,18 @@ def queryExpansion(query, threads=1):
   
   for i in range(0, threads):
     threadPool[i].join()
+  
+  sortedChiSquaredVals = [(chiSquared, term) for chiSquared, term in sorted(zip(chiSquaredVals, termsList), reverse=True)][0:30]
+  log("time", "Finished performing Query Expansion in " + str(time.time() - startTime) + ' seconds.')
+
+  return sortedChiSquaredVals
+
+
+#TODO - Copy this from earlier commits. This stores the inverted index in memory for actually really fast calculation.
+#Should probably move forward with this method if we want to use it. Threading is wayyyy too slow.
+def unthreadedQE(query, terms):
+  pass
 
 if __name__ == "__main__":
-  # characters = ['(', '.', '$']
-  #add numbers and letters using for loop
-  # regex = re.compile('^'+'a', re.IGNORECASE)
-  # terms = InvertedIndex.objects(term=regex)
-  
-  terms = json.loads(InvertedIndex.objects.order_by('-term').to_json())
-
-  queryExpansion('chicken', terms)
-  # queryExpansion('butter', terms)
+  print(queryExpansion('chicken', 4))
+  print(queryExpansion('butter', 4))
