a9c7daba103f62479188b8a995eb715783c98991
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-23 22:59:30 -0400

    Wrote foundation for threading

--- ranker/queryExpansion.py
+++ ranker/queryExpansion.py
@@ -8,16 +8,22 @@ from helpers import log
 import rankerDBConfig
 import re
 import json
+from threading import Thread
 
 connect(rankerDBConfig.databaseName, host=rankerDBConfig.databaseAddr, port=27017)
 N = Crawler.objects.count()
+characters = ['(', '.', '$']
+for i in range(0, 9):
+  characters.append(str(i))
+for i in range(ord('a'), ord('z')+1):
+  characters.append(chr(i))
 
-def queryExpansion(query, terms):
-  startTime = time.time()
-  log("QE", 'Performing Query Expansion on ' + query)
+def queryExpansionWorker(queryDocInfo, terms):
+  # startTime = time.time()
+  # log("QE", 'Performing Query Expansion on ' + query)
 
-  queryTerm = json.loads(InvertedIndex.objects.get(term=query).to_json())
-  queryDocInfo = queryTerm['doc_info']
+  # queryTerm = json.loads(InvertedIndex.objects.get(term=query).to_json())
+  # queryDocInfo = queryTerm['doc_info']
 
   Na = len(queryDocInfo)
   Nab, Nb = 0, 0
@@ -40,6 +46,24 @@ def queryExpansion(query, terms):
   sortedChiSquaredVals = [(chiSquared, term) for chiSquared, term in sorted(zip(chiSquaredVals, termsList), reverse=True)][0:30]
   log("time", "Finished performing Query Expansion in " + str(time.time() - startTime) + ' seconds.')
   return sortedChiSquaredVals
+
+def queryExpansion(query, threads=1):
+  startTime = time.time()
+  log("QE", 'Performing Query Expansion on ' + query)
+
+  queryTerm = json.loads(InvertedIndex.objects.get(term=query).to_json())
+  queryDocInfo = queryTerm['doc_info']
+
+  threadPool = []
+  for i in range(0, threads):
+    newThread = Thread(name='queryExpansion_'+str(i), target=queryExpansionWorker, args=(queryDocInfo))
+    threadPool.append(newThread)
+  
+  for i in range(0, threads):
+    threadPool[i].start()
+  
+  for i in range(0, threads):
+    threadPool[i].join()
 
 if __name__ == "__main__":
   # characters = ['(', '.', '$']
