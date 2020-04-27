3c1944f470c41efd31576f4ddb6317ed93accf4a
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-27 03:29:30 -0400

    Code cleanup

--- ranker/loadInvertedIndexToMemory.py
+++ ranker/loadInvertedIndexToMemory.py
@@ -7,22 +7,6 @@ from mongoengine import *
 from mongoConfig import *
 from helpers import log
 
-
-# def loadInvertedIndexToMemory():
-#   log('info', 'Loading Inverted Index into main memory.')
-#   startTime=time.time()
-
-#   invertedIndex = InvertedIndex.objects()
-#   allTerms = [(term['doc_info'], term['term']) for term in invertedIndex]
-  
-#   termReverseMap = {}
-#   for i in range(0, len(allTerms)):
-#     termReverseMap[allTerms[i][1]] = i
-
-#   log('time', 'Finished loading Inverted Index into main memory in ' + str(time.time()-startTime) + ' seconds.')
-#   return termReverseMap, invertedIndex
-
-# ----------------------- END OF OLD LOAD INVERTED INDEX ------------------------
 def loadInvertedIndexToMemory():
   log('info', 'Loading Inverted Index into main memory.')
   startTime=time.time()
