1930cf457f5cbdaae28601d1fd79f01f6de571eb
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-28 23:48:19 -0400

    Updated recommender initialize

--- recommender/initialize.py
+++ recommender/initialize.py
@@ -1,3 +1,14 @@
+import sys
+sys.path.append('..')
+from helpers import *
+import time
+
 def initialize():
   # Step 1. Get all users and their likes and dislikes.
-  pass
+  startTime = time.time()
+  log('info', 'Fetching all user info from client server')
+  response = makeRequest('client', '/external/allUsers')
+  print(response['content'])
+
+  log('time', 'Finished fetching all user info in  ' + str(time.time() - startTime) + ' seconds')
+
